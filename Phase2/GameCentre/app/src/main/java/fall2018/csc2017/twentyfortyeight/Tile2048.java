f1005f25f7a4e781d2a325cce2bf2e2aa73c8cac
MD Tanvir Hyder <mdtanvir.hyder@mail.utoronto.ca>
2018-11-16 19:04:55 -0500

    edited Tile2048 class, renamed image names

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/twentyfortyeight/Tile2048.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/twentyfortyeight/Tile2048.java
@@ -1,33 +1,52 @@
-package fall2018.csc2017.twentyfortyeight;
+package fall2018.csc2017.twentyfortyeight.;
 
 import android.support.annotation.NonNull;
 import java.io.Serializable;
 
-import fall2018.csc2017.slidingtiles.R;
-
 public class Tile2048 implements Comparable<Tile2048>, Serializable {
 
+    /**
+     * The background refers to image file
+     */
     private int background;
+
+    /**
+     * the value of the image file
+     */
     private int value;
 
-    public int getBackground() {
+    /**
+     * returns the background value
+     *
+     * @return the background value
+     */
+    public int getBackground(){
         return background;
     }
 
-    public int getValue() {
+    /**
+     * returns the value
+     *
+     * @return the value
+     */
+    public int getValue(){
         return value;
     }
 
-    // Constructor
-    Tile2048(int value, int background) {
+
+    Tile2048(int value, int background){
         this.value = value;
         this.background = background;
     }
 
-    Tile2048(int backgroundId) {
-        value = backgroundId + 1;
-        Integer[] tileIds = {R.drawable.tile_blank};
-        background = tileIds[backgroundId];
+    Tile2048(int backgroundID){
+        Integer[] tileIds = {R.drawable.tile_1, R.drawable.tile_2, R.drawable.tile_3,
+                R.drawable.tile_4, R.drawable.tile_5, R.drawable.tile_6, R.drawable.tile_7,
+                R.drawable.tile_8, R.drawable.tile_9, R.drawable.tile_10, R.drawable.tile_11,
+                R.drawable.tile_12, R.drawable.tile_13, R.drawable.tile_14, R.drawable.tile_15,
+                R.drawable.tile_16, R.drawable.tile_17, R.drawable.tile_18, R.drawable.tile_19,
+                R.drawable.tile_20, R.drawable.tile_21, R.drawable.tile_22, R.drawable.tile_23,
+                R.drawable.tile_24, R.drawable.tile_blank};
     }
 
     @Override
