bc227e329d76443820287a4c95ddeabfd3aa10cd
MD Tanvir Hyder <mdtanvir.hyder@mail.utoronto.ca>
2018-11-19 23:22:18 -0800

    implemented Tile2048

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/twentyfortyeight/Tile2048.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/twentyfortyeight/Tile2048.java
@@ -1,71 +1,38 @@
 package fall2018.csc2017.twentyfortyeight;
 
-import android.support.annotation.NonNull;
 import java.io.Serializable;
-
+import fall2018.csc2017.abstractClasses.AbstractTile;
 import fall2018.csc2017.slidingtiles.R;
 
-public class Tile2048 implements Comparable<Tile2048>, Serializable {
+/**
+ * A 2048 game tile with id as the value of the tile
+ * and background being the image reference of the tile
+ */
+public class Tile2048 extends AbstractTile implements Comparable<AbstractTile>, Serializable {
 
     /**
-     * The background refers to image file
-     */
-    private int background;
-
-    /**
-     * the value of the image file
-     */
-    private int value;
-
-    /**
-     * returns the background value
+     * A Tile with id and background. The background may not have a corresponding image.
      *
-     * @return the background value
-     */
-    public int getBackground(){
-        return background;
-    }
-
-    /**
-     * returns the value
-     *
-     * @return the value
-     */
-    public int getValue(){
-        return value;
-    }
-
-    /**
-     * A tile2048 with value and background
-     *
-     * @param value      the value
+     * @param id         the id
      * @param background the background
      */
-    Tile2048(int value, int background){
-        this.value = value;
-        this.background = background;
+    Tile2048(int id, int background) {
+        super(id, background);
     }
 
     /**
-     * A tile2048 with a background id; looks up and set the background
-     *
-     * @param backgroundID the background id
+     * A tile with a id being the value of the tile
+     * Precondition: id must be a power of 2 where id values ranges from 0 to 2048
+     * @param id the value of the tile
      */
-    Tile2048(int backgroundID){
-        if (backgroundID != 11) {
-            value = (int) Math.pow(2, (double) (backgroundID + 1));
-        } else {
-            value = 0;
-        }
+    Tile2048(int id) {
+        super(id);
+        int backgroundID = id == 0 ? 11 : (int) (Math.log((double) id)/Math.log((double) 2)) - 1;
+        //Array for 2048 tile Ids
         Integer[] tileIds = {R.drawable.tile2048_2, R.drawable.tile2048_4, R.drawable.tile2048_8,
                 R.drawable.tile2048_16, R.drawable.tile2048_32, R.drawable.tile2048_64,
                 R.drawable.tile2048_128, R.drawable.tile2048_256, R.drawable.tile2048_512,
                 R.drawable.tile2048_1024, R.drawable.tile2048_2048, R.drawable.tile_blank};
         background = tileIds[backgroundID];
-    }
-
-    @Override
-    public int compareTo(@NonNull Tile2048 o) {
-        return o.value - this.value;
     }
 }
